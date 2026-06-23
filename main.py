9caf877aff19d31d6826c3f28d392fcb0b6dc7cb
averylhammond <149446626+averylhammond@users.noreply.github.com>
2026-06-23 12:40:53 -0500

    refactor source into source/ folder

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import os, logging, tabula, re, win32gui, win32con
 import PySimpleGUI as sg
-from InventoryEntry import *
-from TurnoverEntry import *
-from spreadsheetDriver import *
+from source.InventoryEntry import *
+from source.TurnoverEntry import *
+from source.spreadsheetDriver import *
 
 # Uncomment these lines when running pyinstaller to hide the windows terminal
 # upon program execution
-#hide = win32gui.GetForegroundWindow()
-#win32gui.ShowWindow(hide, win32con.SW_HIDE)
+# hide = win32gui.GetForegroundWindow()
+# win32gui.ShowWindow(hide, win32con.SW_HIDE)
 
 # Get cwd of executable plus folder that contains PDFs
 inventoryDir = os.getcwd() + "/InventoryAvailability/"
 turnoverDir = os.getcwd() + "/TurnoverReports/"
+
 
 # processInventoryPage is responsibile for parsing a page worth of PDF table data
 # and converting each line into an InventoryEntry object
@@ -23,28 +24,29 @@ def processInventoryPage(page, inventoryTable):
 
     # Loop through all lines in the page but skip first two lines that contain header info
     for line in page.splitlines()[2:]:
-        
+
         # Use double space to separate white space from strings
         data = line.split("  ")
-        
+
         # Strip out excess spaces
-        data = [i for i in data if i != '']
+        data = [i for i in data if i != ""]
 
         # Case: This row is the first, contains all data to populate an entry
-        if data[-1] != 'NaN':
+        if data[-1] != "NaN":
             entry = InventoryEntry()
             entry.populateInventoryEntry(data)
             inventoryTable.append(entry)
 
         # Case: Not the first row, see if part or description data needs to be updated
         else:
-            if data[1].lstrip() != 'NaN':
+            if data[1].lstrip() != "NaN":
                 inventoryTable[-1].part += data[1]
-            if data[2].lstrip() != 'NaN':
+            if data[2].lstrip() != "NaN":
                 inventoryTable[-1].description += data[2]
 
     # Return up to date list with new entries
     return inventoryTable
+
 
 # processInventoryFile processes a given inventory table by splitting the pdf into
 # pages and processing each page
@@ -52,7 +54,7 @@ def processInventoryPage(page, inventoryTable):
 # returns: inventoryTable[], list of InventoryEntry class objects pertaining
 # to filenames inventory entries
 def processInventoryFile(filepath):
-    
+
     # Table that will contain InventoryEntry objects
     inventoryTable = []
 
@@ -77,23 +79,23 @@ def processInventoryFile(filepath):
 # returns: N/A
 def buildCheckboxDict(values):
     return {
-        "Part" : True,  # Always include part 
-        "Description" : values["-DESCRIPTION-"],
-        "UOM" : values["-UOM-"],
-        "OnHand" : values["-ONHAND-"],
-        "Allocated" : values["-ALLOCATED-"],
-        "NotAvailable" : values["-NOTAVAILABLE-"],
-        "DropShip" : values["-DROPSHIP-"],
-        "Available" : values["-AVAILABLE-"],
-        "OnOrder" : values["-ONORDER-"],
-        "Committed" : values["-COMMITTED-"],
-        "Short" : values["-SHORT-"],
-        "tDescription" : values["-TDESCRIPTION-"],
-        "tUnits Sold" : values["-TUNITSSOLD-"],
-        "tAvg QOH" : values["-TQOH-"],
-        "tAvg TO Days" : values["-TAVGTODAYS-"],
-        "tTO Rate" : values["-TTORATE-"]
-     }
+        "Part": True,  # Always include part
+        "Description": values["-DESCRIPTION-"],
+        "UOM": values["-UOM-"],
+        "OnHand": values["-ONHAND-"],
+        "Allocated": values["-ALLOCATED-"],
+        "NotAvailable": values["-NOTAVAILABLE-"],
+        "DropShip": values["-DROPSHIP-"],
+        "Available": values["-AVAILABLE-"],
+        "OnOrder": values["-ONORDER-"],
+        "Committed": values["-COMMITTED-"],
+        "Short": values["-SHORT-"],
+        "tDescription": values["-TDESCRIPTION-"],
+        "tUnits Sold": values["-TUNITSSOLD-"],
+        "tAvg QOH": values["-TQOH-"],
+        "tAvg TO Days": values["-TAVGTODAYS-"],
+        "tTO Rate": values["-TTORATE-"],
+    }
 
 
 # processTurnoverPage is responsibile for parsing a page worth of PDF table data
@@ -104,18 +106,18 @@ def buildCheckboxDict(values):
 # pdf table entries including the page that was just processed
 def processTurnoverPage(page, turnoverTable):
 
-    i=0
+    i = 0
     # Loop through all lines in the page but skip first two lines that contain header info
     for line in page.splitlines():
-        
+
         # Use double space to separate white space from strings
         data = line.split("  ")
-        
+
         # Strip out excess spaces
-        data = [i for i in data if i != '']
+        data = [i for i in data if i != ""]
 
         # Second word will always contain 'Totals:' if a valid total line
-        if 'Totals:' in data[1]:
+        if "Totals:" in data[1]:
             entry = TurnoverEntry()
             entry.populateTurnoverEntry(data)
             turnoverTable.append(entry)
@@ -145,14 +147,14 @@ def processTurnoverFile(filepath):
     # Update inventoryTable with each new page processed
     for page in data:
         turnoverTable = processTurnoverPage(page.to_string(), turnoverTable)
-    
+
     if __debug__:
         for i in turnoverTable:
             i.dumpTurnoverEntry()
 
     return turnoverTable
-    
-    
+
+
 # run is the main loop of the program.
 # params: N/A
 # returns: N/A
@@ -163,24 +165,59 @@ def run():
 
     # Define GUI layout
     layout = [
-        [sg.Text("Choose an Inventory Availability PDF to process...")],  # First text window
-        [sg.InputText(key = "-FILE_PATH-"), sg.FileBrowse(initial_folder=inventoryDir, file_types=[("PDF Files", "*.pdf")])],
-        [sg.Text("Please check all INVENTORY elements that you would like to have included on the report:")],  # First text window
-        [sg.Checkbox("Description", key="-DESCRIPTION-"), sg.Checkbox("UOM", key="-UOM-"), sg.Checkbox("On Hand", key="-ONHAND-"), sg.Checkbox("Allocated", key="-ALLOCATED-")],
-        [sg.Checkbox("Not Available", key="-NOTAVAILABLE-"), sg.Checkbox("Drop Ship", key="-DROPSHIP-"), sg.Checkbox("Available", key="-AVAILABLE-")],
-        [sg.Checkbox("On Order", key="-ONORDER-"), sg.Checkbox("Committed", key="-COMMITTED-"), sg.Checkbox("Short", key="-SHORT-")],
-        [sg.Text("Please check all TURNOVER elements that you would like to have included on the report:")],  # Second text window
-        [sg.Checkbox("Description", key="-TDESCRIPTION-"), sg.Checkbox("Units Sold", key="-TUNITSSOLD-"), sg.Checkbox("Avg QOH", key="-TQOH-")],
-        [sg.Checkbox("Avg TO Days", key="-TAVGTODAYS-"), sg.Checkbox("TO Rate", key="-TTORATE-")],
+        [
+            sg.Text("Choose an Inventory Availability PDF to process...")
+        ],  # First text window
+        [
+            sg.InputText(key="-FILE_PATH-"),
+            sg.FileBrowse(
+                initial_folder=inventoryDir, file_types=[("PDF Files", "*.pdf")]
+            ),
+        ],
+        [
+            sg.Text(
+                "Please check all INVENTORY elements that you would like to have included on the report:"
+            )
+        ],  # First text window
+        [
+            sg.Checkbox("Description", key="-DESCRIPTION-"),
+            sg.Checkbox("UOM", key="-UOM-"),
+            sg.Checkbox("On Hand", key="-ONHAND-"),
+            sg.Checkbox("Allocated", key="-ALLOCATED-"),
+        ],
+        [
+            sg.Checkbox("Not Available", key="-NOTAVAILABLE-"),
+            sg.Checkbox("Drop Ship", key="-DROPSHIP-"),
+            sg.Checkbox("Available", key="-AVAILABLE-"),
+        ],
+        [
+            sg.Checkbox("On Order", key="-ONORDER-"),
+            sg.Checkbox("Committed", key="-COMMITTED-"),
+            sg.Checkbox("Short", key="-SHORT-"),
+        ],
+        [
+            sg.Text(
+                "Please check all TURNOVER elements that you would like to have included on the report:"
+            )
+        ],  # Second text window
+        [
+            sg.Checkbox("Description", key="-TDESCRIPTION-"),
+            sg.Checkbox("Units Sold", key="-TUNITSSOLD-"),
+            sg.Checkbox("Avg QOH", key="-TQOH-"),
+        ],
+        [
+            sg.Checkbox("Avg TO Days", key="-TAVGTODAYS-"),
+            sg.Checkbox("TO Rate", key="-TTORATE-"),
+        ],
         [sg.Button("Process This Inventory"), sg.Exit()],  # Exit button
-        [output]  # Output text window
+        [output],  # Output text window
     ]
 
     # Set theme for big style
     sg.theme("LightGreen5")
 
     # Create window
-    window = sg.Window("Automated Inventory Processor", layout, size=(650,500))
+    window = sg.Window("Automated Inventory Processor", layout, size=(650, 500))
 
     # Main program loop
     while True:
@@ -197,7 +234,7 @@ def run():
             # TODO: Revise this if else structure, don't like it
             if values["-FILE_PATH-"] == "":
                 output.update("Please choose a valid Inventory Availability PDF file!")
-            
+
             else:
                 # Update output text
                 output.update("Processing Inventory... Please wait.")
@@ -210,11 +247,15 @@ def run():
                 inventory = processInventoryFile(values["-FILE_PATH-"])
 
                 # Get the date of the inventory file from the name. This will be the name of the excel file
-                filename = (re.search("(\d*).pdf", values["-FILE_PATH-"])).group().replace(".pdf", "")
-            
+                filename = (
+                    (re.search("(\d*).pdf", values["-FILE_PATH-"]))
+                    .group()
+                    .replace(".pdf", "")
+                )
+
                 # Spreadsheet Workbook definition
                 workbook = xlsxwriter.Workbook(filename + ".xlsx")
-                
+
                 # Setup a spreadsheet with the inventory availability
                 nextCol = setupMainSpreadsheet(workbook, inventory, checkboxDict)
 
@@ -226,26 +267,32 @@ def run():
                 for file in os.listdir(turnoverDir):
                     turnover = processTurnoverFile(turnoverDir + file)
 
-                    setupSpreadsheetTurnoverHeader(workbook, checkboxDict, nextCol, file.replace('.pdf', ''))
+                    setupSpreadsheetTurnoverHeader(
+                        workbook, checkboxDict, nextCol, file.replace(".pdf", "")
+                    )
 
                     # Append turnover data to columns in workbook
-                    appendTurnoverToSpreadsheet(workbook, turnover, inventory, nextCol, checkboxDict)
+                    appendTurnoverToSpreadsheet(
+                        workbook, turnover, inventory, nextCol, checkboxDict
+                    )
                     nextCol += 1
 
                 # Save and close the spreadsheet
                 workbook.close()
-            
+
                 output.update("Successfully processed Inventory Availability!")
 
     # If break, close app
     window.close()
 
-        
+
 # Entry point
 if __name__ == "__main__":
 
     # Setup logging
-    logging.basicConfig(level = logging.DEBUG, format = "[%(levelname)s] %(asctime)s - %(message)s")
+    logging.basicConfig(
+        level=logging.DEBUG, format="[%(levelname)s] %(asctime)s - %(message)s"
+    )
 
     # Setup and run main program loop
     run()
